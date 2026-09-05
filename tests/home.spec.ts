eb8009b6a90effd94945f4bbf2b1c762e1b4dd60
Pedro Borges <pedroborgespj@gmail.com>
2026-09-05 15:09:14 -0300

    fixing first test to be title check and adding a new test to check the slogan

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,7 +1,14 @@
 import { test, expect } from '@playwright/test'
 
+test('should verify the title in the browser tab', async ({ page }) => {
+  await page.goto('http://localhost:3000/')
+
+  await expect(page).toHaveTitle("Lunar Pass — Passagens para a Lua")
+})
+
 test('should show the slogan in homepage', async ({ page }) => {
   await page.goto('http://localhost:3000/')
 
-  await expect(page).toHaveTitle("Lunar Pass — Passagens para a Lua");
+  const slogan = page.getByRole('heading', {name: 'Sua viagem para a Lua começa aqui.'})
+  await expect(slogan).toBeVisible()
 })
